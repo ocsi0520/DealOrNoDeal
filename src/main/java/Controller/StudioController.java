719156548c32694f36c8d66a7a716f6b46306e0a
Faragó Tam ás <ocsi0520@gmail.com>
2018-05-01 21:06:27 +0200

    [AMEND ME] Scene builder

--- src/main/java/Controller/StudioController.java
+++ src/main/java/Controller/StudioController.java
@@ -1,9 +1,15 @@
 package Controller;
 
 import Logic.DealOrNoDeal;
+import Model.Bag;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
+
+import javax.swing.text.View;
 
 //https://stackoverflow.com/questions/34785417/javafx-fxml-controller-constructor-vs-initialize-method?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 public class StudioController {
@@ -11,14 +17,21 @@ public class StudioController {
     ViewHelper viewHelper;
     DealOrNoDeal dealOrNoDeal;
     public StudioController(){
-        /*this.dealOrNoDeal=new DealOrNoDeal(
-                new BagService(
-                        new BagDao(
-                                Provider.EntityManagerProvider.provideEntityManager()
-                        )
-                )
-        );
-        this.viewHelper=new ViewHelper(this.dealOrNoDeal.getBags());*/
+        dealOrNoDeal=ApplicationContext.getInstance().getDealOrNoDeal();
+        viewHelper = new ViewHelper(dealOrNoDeal.getBags());
+    }
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    public void imageClicked(MouseEvent event){
+        StackPane selectedControl=((StackPane)event.getSource());
+        update(selectedControl);
+
+        //Text text= (Text) ((StackPane)event.getSource()).getChildren().get(1);
+        //text.setText("aaaaaaa");
     }
     public void proba(MouseEvent event){
         /*
@@ -32,9 +45,32 @@ public class StudioController {
         clickedImage.setDisable(true);
         System.out.println("Mostmár: " + this.dealOrNoDeal.getBags().get(Id).isOpen());
         */
-        Text text= (Text) ((StackPane)event.getSource()).getChildren().get(1);
-        text.setText("");
 
+        StackPane selectedControl=((StackPane)event.getSource());
+        update(selectedControl);
 
+        //Text text= (Text) ((StackPane)event.getSource()).getChildren().get(1);
+        //text.setText("");
+    }
+
+    private void update(int bagIdx, Bag bagData) {
+        // Find bag node
+        StackPane bagNode = null;
+        // ...
+
+        // Update bag node
+        update(bagNode);
+    }
+
+    private void update(StackPane bagNode) {
+        /*int index=Integer.parseInt(bagNode.getId());
+        Bag bag=dealOrNoDeal.getBags().get(index);
+        if(bag.isOpen()){
+            //szöveg
+            //kép
+            ImageView imageView= (ImageView) bagNode.getChildren().get(0);
+            //imageView.set
+        }
+*/
     }
 }
